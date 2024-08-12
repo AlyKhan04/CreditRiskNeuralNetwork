42d87ced17ee8d9d0f23e4d006e53a55f8d86861
AlyKhan04 <alykhan222004@gmail.com>
2024-08-12 14:53:07 +0500

    Added BackPropogation

--- NeuralNetwork/src/main/java/NeuralNetwork.java
+++ NeuralNetwork/src/main/java/NeuralNetwork.java
@@ -22,4 +22,46 @@ public class NeuralNetwork {
 
         return output;  // Return the final output
     }
+
+    // Method to train the network using backpropagation and gradient descent
+    public void train(double[][] X, double[][] y, int epochs, double learningRate) {
+        for (int epoch = 0; epoch < epochs; epoch++) {
+            for (int i = 0; i < X.length; i++) {
+                double[] predicted = predict(X[i]);
+
+                // Calculate the error for the output layer
+                double[] outputError = new double[predicted.length];
+                for (int j = 0; j < predicted.length; j++) {
+                    outputError[j] = predicted[j] * (1 - predicted[j]) * (predicted[j] - y[i][j]);
+                }
+
+                double[][] errors = new double[layers.length][];
+                errors[layers.length - 1] = outputError;
+
+                // Backpropagate the error through the network
+                for (int j = layers.length - 2; j >= 0; j--) {
+                    errors[j] = new double[layers[j].getNeurons().length];
+                    for (int k = 0; k < layers[j].getNeurons().length; k++) {
+                        double error = 0.0;
+                        for (int l = 0; l < layers[j + 1].getNeurons().length; l++) {
+                            error += errors[j + 1][l] * layers[j + 1].getNeurons()[l].getWeights()[k];
+                        }
+                        errors[j][k] = error * layers[j].getNeurons()[k].activate(X[i]) * (1 - layers[j].getNeurons()[k].activate(X[i]));
+                    }
+                }
+
+                // Update the weights and biases using gradient descent
+                for (int j = 0; j < layers.length; j++) {
+                    double[] inputs = (j == 0) ? X[i] : layers[j - 1].forward(X[i]);
+                    for (int k = 0; k < layers[j].getNeurons().length; k++) {
+                        Neuron neuron = layers[j].getNeurons()[k];
+                        for (int l = 0; l < neuron.getWeights().length; l++) {
+                            neuron.getWeights()[l] -= learningRate * errors[j][k] * inputs[l];
+                        }
+                        neuron.setBias(neuron.getBias() - learningRate * errors[j][k]);
+                    }
+                }
+            }
+        }
+    }
 }
