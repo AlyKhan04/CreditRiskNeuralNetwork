a6adc68d909df3cb536ff9c124ea0c1f7b48cb9f
AlyKhan04 <alykhan222004@gmail.com>
2024-08-12 13:10:04 +0500

    [Feature]: Reworked the Neuron class

--- NeuralNetwork/src/main/java/NetworkNeurons.java
+++ NeuralNetwork/src/main/java/NetworkNeurons.java
@@ -1,3 +1,9 @@
-public class NetworkNeurons {
+import java.util.Arrays;
+import java.util.List;
 
+public class NetworkNeurons {
+    List<Neuron> neuronNet = Arrays.asList(
+            new Neuron(), new Neuron(), new Neuron(), /* input node */
+            new Neuron(), new Neuron(),               /* hidden node */
+            new Neuron());                           /* Output Node */
 }
