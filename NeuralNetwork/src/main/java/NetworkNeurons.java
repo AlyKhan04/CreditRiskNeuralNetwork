20fabfc728549c32bc5ecc04734dcb98ff53ec79
AlyKhan04 <alykhan222004@gmail.com>
2024-08-13 14:23:59 +0500

    [Removal]: Gradle Project

--- NeuralNetwork/src/main/java/NetworkNeurons.java
+++
@@ -1,9 +0,0 @@
-import java.util.Arrays;
-import java.util.List;
-
-public class NetworkNeurons {
-    List<Neuron> neuronNet = Arrays.asList(
-            new Neuron(), new Neuron(), new Neuron(), /* input node */
-            new Neuron(), new Neuron(),               /* hidden node */
-            new Neuron());                           /* Output Node */
-}
